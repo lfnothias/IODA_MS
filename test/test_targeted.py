14488d4646636a8d478e950b9f23dae5d6b3514d
lfnothias <louisfelix.nothias@gmail.com>
2020-07-13 14:42:51 -0700

    again

--- test/test_targeted.py
+++ test/test_targeted.py
@@ -11,8 +11,8 @@ from IODA_targeted_workflow import *
 
 ### IODA-targeted-from-mzTabs
 def test_targeted_from_mztab_googledrive():
-    os.sys(pwd)
-    os.sys(ls)
+    os.system('pwd)
+    os.system('ls')
     input_filename = 'https://drive.google.com/file/d/1NGVzhrw-xZ4nMJserIQ7v4tYgcmraZ6g/view?usp=sharing'
     ratio_value = 5
     min_intensity_value = 1E5
@@ -21,8 +21,8 @@ def test_targeted_from_mztab_googledrive():
     make_targeted_list_from_mzTab(input_filename, experiment_number, ratio_value, min_intensity_value)
 
 def test_targeted_from_mztab_local():
-    os.sys(pwd)
-    os.sys(ls)
+    os.system('pwd')
+    os.system('ls')
     input_filename = 'tests/Euphorbia/Targeted/ioda_input/Euphorbia_rogers_latex_Blank_MS1_2uL_to_Euphorbia_rogers_latex_latex_MS1_2uL_mrgd.mzTab'
     ratio_value = 5
     min_intensity_value = 1E5
