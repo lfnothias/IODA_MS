6b1ea4e94a2f11cafd7e847972d7640539892fd2
Mingxun Wang <mwang87@gmail.com>
2020-07-13 10:59:44 -0700

    adding meat to test

--- test/test.py
+++ test/test.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import sys
 
-def test():
-    print("X")
+sys.path.insert(0, "..")
+
+def test_mztab_import():
+    from IODA_exclusion_workflow import *
+
+    input_filename = 'https://drive.google.com/file/d/1LYk-PKsBWl4Pv7c1TlhQwaqwkF2T6sux/view?usp=sharing'
+    min_intensity = 100
+    rt_exclusion_margin = 5
+
+    make_exclusion_from_mzTab(input_filename, min_intensity, rt_exclusion_margin)
